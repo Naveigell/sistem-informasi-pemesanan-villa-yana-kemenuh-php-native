24436285c6a990913956e7533e76615c602c123d
Naveigell <yosua.yerikho123@gmail.com>
2023-05-17 16:55:29 +0800

    hash password before stored to user table

--- public/actions/auth/auth_registration.php
+++ public/actions/auth/auth_registration.php
@@ -6,7 +6,7 @@ require_once '../../../server.php';
 
 $name     = $_POST['name'];
 $email    = $_POST['email'];
-$password = $_POST['password'];
+$password = md5($_POST['password']);
 $phone    = $_POST['phone'];
 $address  = $_POST['address'];
 
